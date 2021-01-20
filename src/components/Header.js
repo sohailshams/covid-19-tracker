22457fec7a8cb71f21b3d97f096ccd5343dd3e16
sohailshams <sohailshamas@yahoo.com>
2021-01-20 22:32:47 +0100

    Added useEffect function - made an api call - got data and populated it dynamically in dropdown to show countries list

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select } from '@material-ui/core';
 
 function Header() {
-  const [countries, setCountries] = useState(['usa', 'uk']);
+  const [countries, setCountries] = useState([]);
 
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then((response) => response.json())
+        .then((data) => {
+          // restructuring of data by looping over the data came from the request
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+
+    getCountriesData();
+  }, []);
   return (
     <div className="app__header">
       <h1>Covid-19 Tracker</h1>
       <FormControl className="app__dropdown">
         <Select variant="outlined" value="abc">
           {countries.map((country) => (
-            <MenuItem value="{country}">{country}</MenuItem>
+            <MenuItem value="{country.value}">{country.name}</MenuItem>
           ))}
         </Select>
       </FormControl>
