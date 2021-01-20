5c8ab43a7f97f1043f5a3630bc7bf8912ca8f03c
sohailshams <sohailshamas@yahoo.com>
2021-01-20 23:02:30 +0100

    Added a onChange function to select a particulat country in from dropdown and a useState

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { MenuItem, FormControl, Select } from '@material-ui/core';
 
 function Header() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -21,13 +22,20 @@ function Header() {
 
     getCountriesData();
   }, []);
+
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app__header">
       <h1>Covid-19 Tracker</h1>
       <FormControl className="app__dropdown">
-        <Select variant="outlined" value="abc">
+        <Select variant="outlined" onChange={onCountryChange} value={country}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
           {countries.map((country) => (
-            <MenuItem value="{country.value}">{country.name}</MenuItem>
+            <MenuItem value={country.value}>{country.name}</MenuItem>
           ))}
         </Select>
       </FormControl>
