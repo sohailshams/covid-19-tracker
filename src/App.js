137407d8b6826df9d02682c948cc95ed61aefe54
sohailshams <sohailshamas@yahoo.com>
2021-01-23 21:43:12 +0100

    Restructured the app component - divided it into two parts - one for stats and map - other in country data and graph and added media queries

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import './App.css';
 import Header from './components/Header';
 import InfoContainer from './components/InfoContainer';
 import Map from './components/Map';
+import { Card, CardContent } from '@material-ui/core';
+
+import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__stats">
-        <InfoContainer title="Coronavirus Cases" cases={1232} total={20000} />
-        <InfoContainer title="Recovered" cases={12352} total={3500} />
-        <InfoContainer title="Deaths" cases={12323} total={45000} />
+      <div className="app__statsmap">
+        <Header />
+        <div className="app__stats">
+          <InfoContainer title="Coronavirus Cases" cases={1232} total={20000} />
+          <InfoContainer title="Recovered" cases={12352} total={3500} />
+          <InfoContainer title="Deaths" cases={12323} total={45000} />
+        </div>
+        <Map />
       </div>
-      <Map />
+      <div className="app__countriesgraph">
+        <Card>
+          <CardContent>
+            <h3>Live Cases by Country</h3>
+            <h3>Worldwide New Cases</h3>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
