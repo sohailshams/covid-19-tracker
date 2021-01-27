4f9c393fc9736d421991e48372b2b565ac996ed5
sohailshams <sohailshamas@yahoo.com>
2021-01-27 23:28:12 +0100

    Created InfoContainer.css and added styling to InfoContainer

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
+import './InfoContainer.css';
 
 function InfoContainer({ title, cases, total }) {
   return (
