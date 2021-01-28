5b973817f417c96f68282b40296bc8ea2361b2d7
sohailshams <sohailshamas@yahoo.com>
2021-01-28 23:16:51 +0100

    Updated media query - add margin-bottom to the map, increased height of scroll bar and added margin-top to the graph

--- src/App.js
+++ src/App.js
@@ -129,7 +129,7 @@ function App() {
           <CardContent>
             <h3>Live Cases by Country</h3>
             <Table countries={tableData} />
-            <h3>Worldwide new {casesType}</h3>
+            <h3 className="app__graphTitle">Worldwide new {casesType}</h3>
             <Graph casesType={casesType} />
           </CardContent>
         </Card>
